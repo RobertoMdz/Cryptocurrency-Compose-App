4839c32c963b01f33deddf8e3ad234a51c877a83
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-10-19 21:29:13 -0500

    create coin model

--- app/src/main/java/com/roberthmdz/cryptocurrency_app/data/data_sources/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/roberthmdz/cryptocurrency_app/data/data_sources/remote/CoinPaprikaApi.kt
@@ -1,4 +1,10 @@
 package com.roberthmdz.cryptocurrency_app.data.data_sources.remote
 
+import retrofit2.http.GET
+
 interface CoinPaprikaApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins()
+
 }
