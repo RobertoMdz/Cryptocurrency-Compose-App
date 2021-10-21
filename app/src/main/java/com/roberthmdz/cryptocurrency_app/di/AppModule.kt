647885eaf292c3c500e8d5bf04376aa993ecb63d
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-10-20 22:15:56 -0500

    Implement dependenciy injectios in AppModule

---
+++ app/src/main/java/com/roberthmdz/cryptocurrency_app/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.roberthmdz.cryptocurrency_app.di
+
+import com.roberthmdz.cryptocurrency_app.common.Constants.BASE_URL
+import com.roberthmdz.cryptocurrency_app.data.data_sources.remote.CoinPaprikaApi
+import com.roberthmdz.cryptocurrency_app.data.repository.CoinRepositoryImpl
+import com.roberthmdz.cryptocurrency_app.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+
+}
